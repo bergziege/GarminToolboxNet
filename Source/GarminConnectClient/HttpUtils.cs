256fe92e4c5d15037952fa320238ddbd4ff88492
Bernd Boruttau <berndnet2000@web.de>
2018-03-08 09:12:13 +0100

    added a 5 second timeout to garmin requests

--- Source/GarminConnectClient/HttpUtils.cs
+++ Source/GarminConnectClient/HttpUtils.cs
@@ -39,6 +39,7 @@ namespace GarminConnectClient
 			request.KeepAlive = true;
 			request.Method = "GET";
 		    request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1";
+		    request.Timeout = 5000;
 			return request;
 		}
 
